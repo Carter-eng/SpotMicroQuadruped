a02e254afc9a0f193bbbc9501dac38b0eddc826a
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2020-07-15 16:11:43 +0100

    Add files via upload

---
+++ servoTest.py
@@ -0,0 +1,24 @@
+import math
+from adafruit_servokit import ServoKit
+from pynput import keyboard
+
+class sevoTest:
+    def __init__(self):
+        self.kit = ServoKit(channels=16)
+        self.kit.servo[0].angle = 90
+        self.kit.servo[1].angle = 90
+        self.kit.servo[2].angle = 90
+
+    def on_press(key):
+        if key.char in ['q']:
+            self.self.kit.servo[0].angle += 5
+        if key.char in ['a']:
+            self.self.kit.servo[0].angle += -5
+        if key.char in ['w']:
+            self.self.kit.servo[1].angle += 5
+        if key.char in ['s']:
+            self.self.kit.servo[1].angle += -5
+        if key.char in ['e']:
+            self.self.kit.servo[2].angle += 5
+        if key.char in ['d']:
+            self.self.kit.servo[2].angle += -5
