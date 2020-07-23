36211ba054645a80eb1006ff722701df98439984
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2020-07-23 10:59:24 -0400

    Add files via upload

--- servoTest.py
+++ servoTest.py
@@ -2,14 +2,12 @@ import math
 from adafruit_servokit import ServoKit
 import time
 
-class sevoTest:
-    def __init__(self):
-        self.kit = ServoKit(channels=16)
-        self.kit.servo[0].angle = 90
-        self.kit.continuous_servo[0].throttle = 1
-        time.sleep(1)
-        self.kit.continuous_servo[0].throttle = -1
-        time.sleep(1)
-        self.kit.continuous_servo[0].throttle = 0
 
-    
+def __init__(self):
+    self.kit = ServoKit(channels=16)
+    self.kit.servo[0].angle = 90
+    self.kit.continuous_servo[0].throttle = 1
+    time.sleep(1)
+    self.kit.continuous_servo[0].throttle = -1
+    time.sleep(1)
+    self.kit.continuous_servo[0].throttle = 0
